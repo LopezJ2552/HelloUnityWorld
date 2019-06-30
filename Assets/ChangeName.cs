7767859c04361a57376d0deb2a93e3383dc453a1
LopezJ2552 <javierlopez@knights.ucf.edu>
2019-06-30 15:31:18 -0400

    First commit

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+           TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+           textmeshPro.SetText("Hello World from Javier Lopez");
+        }
+    }
+}
